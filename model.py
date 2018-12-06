e1381dfebeea8f2d2eb5f9788ba2ee29e7191a5a
mivadi <mmvandiepen@gmail.com>
2018-12-06 11:06:32 +0100

    extra imports

--- model.py
+++ model.py
@@ -10,6 +10,12 @@ import random
 from abc import abstractmethod
 import gym
 
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch import optim
+from neural_nets import QNetwork, ModelNetwork
+
 
 class DynaQ(object):
     """
